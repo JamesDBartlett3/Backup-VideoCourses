0a529669e0e3a5c8451888aa83198c22e9dc109a
JamesDBartlett3 <37491308+JamesDBartlett3@users.noreply.github.com>
2024-11-27 00:05:42 -0600

    Refactor Backup-VideoCourses.ps1 for improved readability and consistency

--- Backup-VideoCourses.ps1
+++ Backup-VideoCourses.ps1
@@ -25,10 +25,12 @@ Function Backup-VideoCourses {
     ForEach-Object {
       $courseUrl = "https://$DomainName/ajax/videos/$_"
       (Invoke-RestMethod -Method GET -Uri $courseUrl -ContentType "application/json;charset=utf-8").data.videos | 
-      Select-Object -Property tutorial, ned_programmanifest, @{Name = 'video_medium'; Expression = {
-        $_.thumbnail_medium.mediaURL.Substring(0, ($_.thumbnail_medium.mediaURL |
+        Select-Object -Property tutorial, ned_programmanifest, @{Name = 'video_medium'; Expression = {
+          $_.thumbnail_medium.mediaURL.Substring(0, ($_.thumbnail_medium.mediaURL |
           Select-String -Pattern '/' -AllMatches |
-          Select-Object -First 4).Matches[3].Index + 1) + "hls.m3u8"}}
+          Select-Object -First 4).Matches[3].Index + 1) + "hls.m3u8" 
+        }
+      }
     }
   
   $tutorial = $courseContent | Select-Object -ExpandProperty tutorial
@@ -38,41 +40,40 @@ Function Backup-VideoCourses {
   
   $targetDirectory = "$((Get-Location).Path)\$AppName"
   
-  if(!(Test-Path $targetDirectory)) {
+  if (!(Test-Path $targetDirectory)) {
     New-Item -Path $targetDirectory -ItemType Directory | Out-Null
   }
   
-  for($i = 0; $i -lt $numberOfVideos; $i++) {
+  for ($i = 0; $i -lt $numberOfVideos; $i++) {
     $courseNumber = $tutorial[$i].tu_num
     $courseTitle = $tutorial[$i].tu_title
-    $sectionNumber = ([string]$ned_programmanifest[$i].sec_num).PadLeft(2,'0')
+    $sectionNumber = ([string]$ned_programmanifest[$i].sec_num).PadLeft(2, '0')
     $sectionTitle = $ned_programmanifest[$i].sec_title
-    $lessonNumber = ([string]$ned_programmanifest[$i].serial_number).PadLeft(2,'0')
+    $lessonNumber = ([string]$ned_programmanifest[$i].serial_number).PadLeft(2, '0')
     $lessonTitle = $ned_programmanifest[$i].vid_title
     $videoURL = $video_medium[$i].video_medium
     
     $filePath = "$courseNumber - $courseTitle⌿$sectionNumber - $sectionTitle⌿$lessonNumber - $lessonTitle"
     
-    $courseFolder = Join-Path -Path $targetDirectory -ChildPath $($filePath.Split('⌿')[0].Split([IO.Path]::GetInvalidFileNameChars()) -Join '')
-    $sectionFolder = Join-Path -Path $courseFolder -ChildPath $($filePath.Split('⌿')[1].Split([IO.Path]::GetInvalidFileNameChars()) -Join '')
-    $videoFileName = Join-Path -Path $sectionFolder -ChildPath $($filePath.Split('⌿')[2].Split([IO.Path]::GetInvalidFileNameChars()) -Join '')
+    $courseFolder = (Join-Path -Path $targetDirectory -ChildPath $($filePath.Split('⌿')[0].Split([IO.Path]::GetInvalidFileNameChars()) -Join '')).Trim()
+    $sectionFolder = (Join-Path -Path $courseFolder -ChildPath $($filePath.Split('⌿')[1].Split([IO.Path]::GetInvalidFileNameChars()) -Join '')).Trim()
+    $videoFileName = (Join-Path -Path $sectionFolder -ChildPath $($filePath.Split('⌿')[2].Split([IO.Path]::GetInvalidFileNameChars()) -Join '')).Trim()
     
     $partialVideoFileName = $videoFileName + ".part.$FileType"
     $completedVideoFileName = $videoFileName + ".$FileType"
     
-    if(!(Test-Path $courseFolder)) {
+    if (!(Test-Path $courseFolder)) {
       Write-Host "Creating folder: $courseFolder...".Replace("$targetDirectory\", '')
       New-Item -ItemType Directory -Path $courseFolder | Out-Null
     }
     
-    if(!(Test-Path $sectionFolder)) {
+    if (!(Test-Path $sectionFolder)) {
       Write-Host "Creating folder: $sectionFolder...".Replace("$targetDirectory\", '')
       New-Item -ItemType Directory -Path $sectionFolder | Out-Null
     }
     
-    if(!(Test-Path $completedVideoFileName)) {
-      
-      if(Test-Path $partialVideoFileName) {
+    if (!(Test-Path $completedVideoFileName)) {
+      if (Test-Path $partialVideoFileName) {
         Write-Host "Deleting partial file from previous failed or interrupted download: $partialVideoFileName".Replace("$targetDirectory\", '')
         Remove-Item -Path $partialVideoFileName -Force
       }
@@ -86,7 +87,5 @@ Function Backup-VideoCourses {
     else {
       Write-Host "$completedVideoFileName already exists. Skipping...".Replace("$targetDirectory\", '')
     }
-  
   }
-  
 }
